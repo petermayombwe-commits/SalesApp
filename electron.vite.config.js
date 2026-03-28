740075d4ac6c39a3bc4ded5be0ea8d12b1a5a76d
petermayombwe-commits <petermayombwe@gmail.com>
2026-03-28 20:50:08 +0300

    Add files via upload

---
+++ electron.vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'electron-vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  main: {
+    build: {
+      rollupOptions: {
+        external: [
+          'better-sqlite3',
+          'bcryptjs',
+          'electron'
+        ]
+      }
+    }
+  },
+  preload: {
+    build: {
+      rollupOptions: {
+        external: ['electron']
+      }
+    }
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'),
+        '@': resolve('src/renderer/src')
+      }
+    },
+    plugins: [react()]
+  }
+})
