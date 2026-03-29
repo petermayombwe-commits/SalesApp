42bc7a3b8b08cc2f6f460387b20f1166c3bf35a7
petermayombwe-commits <petermayombwe@gmail.com>
2026-03-30 00:13:28 +0300

    Add files via upload

---
+++ ipc-index.js
@@ -0,0 +1,27 @@
+import { registerAuthHandlers } from './auth'
+import { registerProductHandlers } from './products'
+import { registerInventoryHandlers } from './inventory'
+import { registerClientHandlers } from './clients'
+import { registerOrderHandlers } from './orders'
+import { registerCreditHandlers } from './credits'
+import { registerStaffHandlers } from './staff'
+import { registerReportHandlers } from './reports'
+import { registerSettingsHandlers } from './settings'
+import { registerForecastHandlers } from './forecast'
+import { registerNotificationHandlers } from './notifications'
+import { registerExpenseHandlers } from './expenses'
+
+export function registerIpcHandlers(ipcMain) {
+  registerAuthHandlers(ipcMain)
+  registerProductHandlers(ipcMain)
+  registerInventoryHandlers(ipcMain)
+  registerClientHandlers(ipcMain)
+  registerOrderHandlers(ipcMain)
+  registerCreditHandlers(ipcMain)
+  registerStaffHandlers(ipcMain)
+  registerReportHandlers(ipcMain)
+  registerSettingsHandlers(ipcMain)
+  registerForecastHandlers(ipcMain)
+  registerNotificationHandlers(ipcMain)
+  registerExpenseHandlers(ipcMain)
+}
